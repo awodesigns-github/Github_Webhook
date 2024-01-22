a123b4507f7514d1766b7278f204927b23f129b4
awodesigns-github <awo.designs1@gmail.com>
2024-01-22 13:30:59 +0300

    My commit message

--- app/Handler/WebhookHandler.php
+++ app/Handler/WebhookHandler.php
@@ -13,6 +13,8 @@ class WebhookHandler extends ProcessWebhookJob
     {
         $headers = $this->webhookCall->headers;
         $payload = $this->webhookCall->payload;
+
+        // Log the webhook call
         Log::info('Github Header: '. json_encode($headers, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
         Log::info('Github Payload: '. json_encode($payload, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
     }
